1df58daa3e2a4fa1ffd00d930a4f9110d48fed8d
ffr00tloops <banuelosdaniel.db@gmail.com>
2022-03-14 13:57:27 +0800

    functional activtyB

--- activityB/home.php
+++ activityB/home.php
@@ -21,12 +21,13 @@ session_start();
       header("Location:login.php"); 
     }
 
-    if(isset($_POST['login'])) {
+    if(isset($_POST['logout'])) {
       logout();
     }
 
     ?>
 
+    <form method="post">
     <div class="border-8 my-52 lg:mx-64 p-5 ">
       <h1 class="text-white text-lg font-bold p-5 bg-emerald-600">User Information Form</h1>
       <div class="bg-white m-5 p-5">
@@ -36,6 +37,7 @@ session_start();
 
       <button type="submit" name="logout" value="logout" class="text-center btn p-3 m-3 rounded-lg text-white btn-primary bg-emerald-600">Logout</button>
     </div>
+  </form>
 
 
 
